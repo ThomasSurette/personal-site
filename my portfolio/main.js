3b693efa9c09228a10fbc9bccff63948f2ceaf63
ThomasSurette <71235068+ThomasSurette@users.noreply.github.com>
2020-10-08 12:57:19 -0400

    added scrolling funtionality to menu

--- my portfolio/main.js
+++ my portfolio/main.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
 		$('.top-nav').toggleClass('open');
 	})
 });
+
+$(document).ready(function () {
+	$('.top-nav .nav-link').on('click', function (){
+		$('.menu-toggler').removeClass('open');
+		$('.top-nav').removeClass('open');
+	})
+});
+
+$(document).ready(function () {
+	$('nav a[href*="#"]').on('click', function (){
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 2000);
+	})
+});
+
+$('#up').on('click', function (){
+	$('html, body').animate( {
+			scrollTop: 0
+	}, 2000);
+});
+
+AOS.init({
+	easing: 'ease',
+	duration: 1800
+});
